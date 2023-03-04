88078a78b61da5c054d59df2e368b7fd5f0a87df
Tomas PROUST <tomas.proust@epitech.eu>
2023-03-04 16:14:03 +0100

    [ADD] freeing of board

--- solver/src/main.c
+++ solver/src/main.c
@@ -1,5 +1,12 @@
 #include "solver.h"
 
+void free_board(int **board)
+{
+    for (int i = 0; i < BOARD_SIZE; i++)
+        free(board[i]);
+    free(board);
+}
+
 int main(void)
 {
     int **board = get_map();
@@ -9,11 +16,13 @@ int main(void)
     if (solve_sudoku(board))
     {
         print_board(board);
+        free_board(board);
         return _EXIT_SUCCESS;
     }
     else
     {
         printf("Unable to solve the Sudoku board.\n");
+        free_board(board);
         return _EXIT_FAILURE;
     }
     return _EXIT_SUCCESS;
